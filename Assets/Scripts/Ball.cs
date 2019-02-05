eb5ae482d882cf38a337c75a468e50f48b2fa7da
rich <rich.canadajr@gmail.com>
2019-02-04 22:06:26 -0600

    Initial commit with some basic code

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private int _direction = 0;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject _player;
+
+    private GameObject _playerCamera;
+
+    private const int PlayerSpeed = 10;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
